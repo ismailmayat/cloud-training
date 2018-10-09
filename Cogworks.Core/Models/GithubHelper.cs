bcbba4315a881f8984322e546dc6f1887c2daa0b
Ismail Mayat <imayat@thecogworks.co.uk>
2018-10-09 14:17:32 +0100

    intial commit of custom stuff

---
+++ Cogworks.Core/Models/GithubHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Octokit;
+
+namespace Cogworks.Core.Models
+{
+    public class GithubHelper
+    {
+        public static IReadOnlyList<Repository> GetRepositories(string organisation)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+
+            return client.Repository.GetAllForOrg(organisation).Result;
+        }
+    }
+}
